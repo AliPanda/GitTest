0ad76819ca674eb030a150df1125e618e8255b18
AliPanda <929261047@qq.com>
2018-08-07 11:14:47 +0800

    IDEA整合GitHub测试

---
+++ src/com/dfec/hello/entity/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.dfec.hello.entity;
+
+/**
+ * Created by lenovo on 2017/12/2.
+ */
+public class HelloWorld {
+    public static  void main(String args[]){
+        System.out.println("HelloWorld");
+    }
+}
